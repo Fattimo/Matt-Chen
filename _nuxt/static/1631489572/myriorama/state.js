23c188d9f4e194bff29929acdf66a308ec10bab5
Fattimo <Fattimo@users.noreply.github.com>
2021-09-12 23:38:59 +0000

    deploy: 5a3aef534f2bdde82255a311865762f21101c352

--- _nuxt/static/1631489572/myriorama/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1631489572",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmyriorama",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f6c50d40"}}}}(null));
---
+++ _nuxt/static/1631489938/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fmyriorama","\u002Fpassaux"]})
