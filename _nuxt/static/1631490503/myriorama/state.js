6bea22f91beba56be18bb17734fba3defc53df25
Fattimo <Fattimo@users.noreply.github.com>
2021-09-12 23:48:33 +0000

    deploy: ea4fd9d8126e51210137bb5a3fc086c18ead1f9c

--- _nuxt/static/1631490503/myriorama/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FMatt-Chen\u002F_nuxt\u002Fstatic\u002F1631490503",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmyriorama",config:{_app:{basePath:"\u002FMatt-Chen\u002F",assetsPath:"\u002FMatt-Chen\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"fe70cda2"}}}}(null));
